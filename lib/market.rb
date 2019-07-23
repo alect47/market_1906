f38bbfc2aa40984b8a6b580e418e3b9c00abb4a3
Alec Wells <alec.wells47@gmail.com>
2019-07-23 17:29:52 -0600

    Finish iteration 3

--- lib/market.rb
+++ lib/market.rb
@@ -17,4 +17,22 @@ class Market
   def vendors_that_sell(item)
     @vendors.find_all {|ven| ven.check_stock(item) > 0}
   end
+
+  def sorted_item_list
+    @vendors.map {|ven| ven.inventory.keys}
+      .flatten.uniq.sort
+  end
+
+  def total_inventory
+    hash = Hash.new(0)
+    @vendors.each do |ven|
+      sorted_item_list.each do |item|
+        if ven.inventory[item] != nil
+          hash[item] += ven.inventory[item]
+        end
+      end
+    end
+    hash
+  end
+
 end
